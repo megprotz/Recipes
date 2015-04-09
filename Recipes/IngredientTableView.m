4c58ed6871e77f50260b4d1ac182441ff42473d0
mmp001 <mmp001@mathcslab11.mcdaniel.edu>
2015-04-09 14:32:26 -0400

    Got two table views displaying with fake data.

--- Recipes/IngredientTableView.m
+++ Recipes/IngredientTableView.m
@@ -2,7 +2,7 @@
 //  IngredientTableView.m
 //  Recipes
 //
-//  Created by mmp001 on 4/8/15.
+//  Created by mmp001 on 4/9/15.
 //  Copyright (c) 2015 mmp001. All rights reserved.
 //
 
--- Recipes/ViewController.m
+++ Recipes/ViewController.m
@@ -14,14 +14,63 @@
 
 @implementation ViewController
 
+NSArray *tableData;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView==self.categoryTable) {
+        return [tableData count];
+    }
+    else{ //tableView==self.ingredientTable
+        return 1;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView==self.categoryTable) {
+        
+        static NSString *simpleTableIdentifier = @"SimpleTableItem";
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+        
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+        }
+        
+        cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+        return cell;
+    }
+    else{ //tableView==self.ingredientTable
+       static NSString *simpleTableIdentifier = @"SimpleTableItem";
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+        
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+        }
+        
+        cell.textLabel.text = @"hello";
+        return cell;
+
+    }
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView==self.categoryTable) {
+        [self.ingredientTable setHidden:NO];
+    }
+
 }
 
 
